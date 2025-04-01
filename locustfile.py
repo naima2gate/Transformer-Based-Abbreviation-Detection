742fec3851c8b87435acc345efeeabd43a5d0f4f
Naima Rashid <65304156+naima2gate@users.noreply.github.com>
2025-04-01 08:55:28 +0100

    Locust Load Testing

---
+++ locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+import json
+
+class AbbreviationDetectionUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def detect_abbreviation(self):
+        input_text = "NER EPI stem cell transplantation (SCT)"
+        data = {"text": input_text}
+
+        response = self.client.post("/predict", json=data)
+
+        assert response.status_code == 200
+
+        result = json.loads(response.text)
+        predictions = result['predictions']
+        print(predictions)
+
+if __name__ == "__main__":
+    import os
+    os.system("locust -f locustfile.py --host http://0.0.0.0:5001")
